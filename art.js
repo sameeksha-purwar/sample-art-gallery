6ce80ad1dfc671062ee235ba673de2fbbff3a4cf
sameeksha-purwar <69745044+sameeksha-purwar@users.noreply.github.com>
2021-08-13 00:22:34 +0530

    Add files via upload

---
+++ art.js
@@ -0,0 +1,10 @@
+
+	function validate(){
+    var name = document.getElementById("name").value;
+    var phone = document.getElementById("phone").value;
+    var  email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+
+	
+	}
